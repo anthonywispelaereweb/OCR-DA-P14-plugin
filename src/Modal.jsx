b2a9f2d54ac80828b003f1c64464bd770576a19c
anthony wispelaere <awispelaere@partner.auchan.fr>
2025-08-04 08:30:31 +0200

    init first publish

---
+++ src/Modal.jsx
@@ -0,0 +1,14 @@
+const Modal = ({ isOpen, onClose }) => {
+    if (!isOpen) return null;
+    
+    return (
+        <div className="modal">
+        
+            <span className="close" onClick={onClose}>
+            &times;
+            </span>
+            <p>Employee Created!</p>
+        </div>
+    );
+    }
+export default Modal;
